d847ddb54d9f180e696b0a5cf24c323945e3f259
SinoMkosi <166750139+SinoMkosi@users.noreply.github.com>
2026-04-13 21:28:59 +0200

    Add files via upload

---
+++ AudioPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Media;
+using System.IO;
+
+namespace MyAssignment
+{
+    public class AudioPlayer
+
+    {
+       public static void PlayGreeting()
+        {
+            SoundPlayer player = new SoundPlayer("greeting.wav");
+            player.PlaySync();
+        }
+    }
+}
+
