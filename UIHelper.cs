d8428b4aa3745f0cce1ffb37cbfd1965ca36ec81
SinoMkosi <166750139+SinoMkosi@users.noreply.github.com>
2026-04-13 21:29:40 +0200

    Add files via upload

---
+++ UIHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAssignment
+{
+    public class UIHelper
+    {
+        public static void ShowLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("=======================================");
+            Console.WriteLine("   CYBERSECURITY AWARENESS BOT");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("   [🔒] Stay Safe Online [🔒]");
+            Console.WriteLine("=======================================");
+
+            Console.ResetColor();
+        }
+    }
+}
