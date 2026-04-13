fec424d5e7323d9b49d5e7092142f261773bd368
SinoMkosi <166750139+SinoMkosi@users.noreply.github.com>
2026-04-13 21:27:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          AudioPlayer.PlayGreeting();
+
+            Chatbot bot = new Chatbot();
+            bot.Start();
+        }
+    }
+}
